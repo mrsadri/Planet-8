7cae678a0861d3e68ef403389f40d2573cbbf0ec
MSadri <sadri.masih@gmail.com>
2018-11-29 18:04:28 +0330

    Initial Commit

---
+++ Planet 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planet 8
+//
+//  Created by MSadri on 11/29/18.
+//  Copyright © 2018 MSadri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
